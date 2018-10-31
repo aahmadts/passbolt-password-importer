c6aff3b1d65c9488fbc1f24ecd024a35f0dcc731
Ahmad2 <ahmad@newsletter2go.com>
2018-10-31 14:45:47 +0100

    enhance finding csrf token

--- webroot/js/script.js
+++ webroot/js/script.js
@@ -8,12 +8,12 @@ $(document).ready(function () {
         if (fileInputs[0].value !== '') {
             $('#startImport').css('display', 'none');
 
-            let csrfTokenPos = document.cookie.match(/csrfToken=/).index;
-            let csrfToken = document.cookie.slice(csrfTokenPos + 10);
+            let csrfToken = document.cookie.match(/csrfToken=\S+;/)[0];
+            let csrfTockenValue = csrfToken.slice(10, -1);
 
             $(this).ajaxSubmit({
                 "headers": {
-                    "X-CSRF-Token" : csrfToken,
+                    "X-CSRF-Token" : csrfTockenValue,
                 },
                 success: viewResponse
             });
